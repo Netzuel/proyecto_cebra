44425cd5e31134642c26136ff94396222b72041e
Netzuel <56087693+Netzuel@users.noreply.github.com>
2021-01-14 23:56:00 +0100

    Add files via upload

--- server.R
+++ server.R
@@ -13,16 +13,19 @@ users <- list(
 
 text <- reactiveValues(text = "inicializado")
 
-datos_raw <- reactive({read.csv("database.csv")})
-datos_raw()$Fecha <- ymd(datos_raw()$Fecha)
-colnames(datos_raw()) <- c("Nombre", "Apellidos", "Fecha", "F1.5", "F3", "F6", "F12", "F18")
-datos <- reactiveValues(datos = datos_raw())
-
 server <- function(input, output) {
+  #datos_base <- datos_raw()
+  #datos_base$Fecha <- ymd(datos_base$Fecha)
+  #colnames(datos_base) <- c("Nombre", "Apellidos", "Fecha", "F1.5", "F3", "F6", "F12", "F18")
+  datos <- reactiveValues(datos = read.csv("database.csv"))
+  #datos$datos$Fecha <- ymd(datos$datos$Fecha)
+  #colnames(datos$datos) <- c("Nombre", "Apellidos", "Fecha", "F1.5", "F3", "F6", "F12", "F18")
+  
   datos_raw_fecha_string <- reactive({
     data <- datos$datos
     data <- data[-1,]
     colnames(data) <- c("Nombre", "Apellidos", "Fecha", "F1.5", "F3", "F6", "F12", "F18")
+    data$Fecha <- ymd(data$Fecha)
     data <- data %>% arrange(desc(Fecha))
     data$Fecha <- unlist(lapply(data$Fecha, toString))
     return(data)
@@ -78,17 +81,21 @@ server <- function(input, output) {
       else{
         if(grepl("\\.csv$", infile$datapath) == TRUE){
           prueba <- read.csv(infile$datapath)
+          colnames(prueba) <- c("Nombre", "Apellidos", "Fecha", "F1.5", "F3", "F6", "F12", "F18")
+          prueba$Fecha <- ymd(prueba$Fecha)
           if(ncol(prueba) == 8){
             if(sum(colnames(prueba) == colnames(datos$datos)) == ncol(prueba)){
               if(nrow(prueba) >= 1){
                 verifier <- 0
                 for(i in seq_len(nrow(prueba))){
-                  if(is.character(prueba[i,][[1]]) & is.character(prueba[i,][[2]]) & is.character(prueba[i,][[3]]) & is.numeric(prueba[i,][[4]]) & is.numeric(prueba[i,][[5]]) & is.numeric(prueba[i,][[6]]) & is.numeric(prueba[i,][[7]]) & is.numeric(prueba[i,][[8]])){
+                  if(is.character(prueba[i,][[1]]) & is.character(prueba[i,][[2]]) & is.Date(prueba[i,][[3]]) & is.numeric(prueba[i,][[4]]) & is.numeric(prueba[i,][[5]]) & is.numeric(prueba[i,][[6]]) & is.numeric(prueba[i,][[7]]) & is.numeric(prueba[i,][[8]])){
                     verifier <- verifier + 1
                   }
                 }
                 if(verifier == nrow(prueba)){
                   datos_new <- read.csv(infile$datapath)
+                  colnames(datos_new) <- c("Nombre", "Apellidos", "Fecha", "F1.5", "F3", "F6", "F12", "F18")
+                  datos_new$Fecha <- ymd(datos_new$Fecha)
                   datos_new <- unique(rbind(datos$datos, datos_new))
                   datos$datos <- datos_new
                   write.csv(datos_new, "database.csv", row.names = FALSE)
